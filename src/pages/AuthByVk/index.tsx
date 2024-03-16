22840764a9d353e409046891736a26266af30bfd
harr1er <harr1er.work23@gmail.com>
2024-03-16 16:08:17 +0300

    fix vl auth

--- src/pages/AuthByVk/index.tsx
+++ src/pages/AuthByVk/index.tsx
@@ -1,15 +1,31 @@
+import axios from "axios";
 import React from "react";
 
 
 import VK, { Auth } from "react-vk";
+import { useAppDispatch } from "../../store/store";
+import { setUser } from "../../store/user/userSlice";
 
 const AuthByVk = () => {
+    const dispatch = useAppDispatch();
   return (
     <VK apiId={51878430} className="custom-vk-button">
       <Auth
         options={{
-          onAuth: (user: any) => {
-            console.log(user);
+          onAuth: async(user: any) => {
+            const { data } = await axios.post(
+                "https://9854dac21e0f0eee.mokky.dev/login",
+                { id: user.uid, name: user.first_name, email: "", password: ""}
+              );
+            console.log(data);
+             // dispatch(setUser({
+            //     token: "",
+            //     data: {
+            //         id: user.uid,
+            //         name: user.first_name,
+            //         email: "",
+            //     }
+            // }));
           },
         }}
       />
