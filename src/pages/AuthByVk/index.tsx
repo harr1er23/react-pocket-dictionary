d45e1c589483b3a3bc295ad59d848291703d4849
harr1er <harr1er.work23@gmail.com>
2024-03-16 16:45:54 +0300

    fix vk auth

--- src/pages/AuthByVk/index.tsx
+++ src/pages/AuthByVk/index.tsx
@@ -1,35 +1,42 @@
 import axios from "axios";
 import React from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-
 
 import VK, { Auth } from "react-vk";
 import { useAppDispatch } from "../../store/store";
 import { setUser } from "../../store/user/userSlice";
 
 const AuthByVk = () => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   return (
     <VK apiId={51878430} className="custom-vk-button">
       <Auth
         options={{
-          onAuth: async(user: any) => {
-            console.log(user);
-            const { data } = await axios.post(
+          onAuth: async (user: any) => {
+            try {
+              const { data } = await axios.post(
                 "https://9854dac21e0f0eee.mokky.dev/register",
-                {name: user.first_name, email: "", password: ""}
+                { name: user.first_name, email: "", password: "" }
               );
-            console.log(data);
-            // dispatch(setUser({
-            //     token: "",
-            //     data: {
-            //         id: user.uid,
-            //         name: user.first_name,
-            //         email: "",
-            //     }
-            // }));
-            navigate("/app/dictionary");
+              dispatch(
+                setUser({
+                  token: data.token,
+                  data: {
+                    id: data.data.id,
+                    name: data.data.name,
+                    email: data.data.email,
+                  },
+                })
+              );
+              navigate("/app/dictionary");
+            } catch (err: any) {
+              console.log(err);
+              if (err.responce.status === 401) {
+                toast.error("Такой пользователь уже сущесвтует!");
+              }
+            }
           },
         }}
       />
