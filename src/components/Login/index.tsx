de06bac0c9add60a83f93e5363e73f65b7e6c514
harr1er <harr1er.work23@gmail.com>
2024-03-18 12:50:14 +0300

    fix vk auth

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -91,11 +91,6 @@ const Login: React.FC<LoginProps> = ({ setIsLogin }) => {
     }
   };
 
-  const onClickAuthByVk = async () => {
-    const resp = await axios.get('https://oauth.vk.com/authorize?client_id=51878430&redirect_uri=https://react-pocket-dictionary.vercel.app/app/dictionary&scope=22&display=page');
-    console.log(resp);
-  }
-
   return (
     <div className="contact-wrapper">
       <header className="login-cta">
@@ -165,11 +160,11 @@ const Login: React.FC<LoginProps> = ({ setIsLogin }) => {
           <h2>Login with your Social Account</h2>{" "}
         </header>
         <ul>
-          <li onClick={() => onClickAuthByVk()}>
+          <li>
             {/* <Link to="/authByVk" className="facebook">
               <VkontacteIco /> Vkontakte
             </Link> */}
-             <a href="" className="facebook">
+             <a href="https://oauth.vk.com/authorize?client_id=51878430&redirect_uri=https://oauth.vk.com/blank.html&scope=22&display=page" className="facebook">
               <VkontacteIco /> Vkontakte
             </a>
           </li>
